f7f799603965456a1b18b146b9250874387a7fb7
Shivangi-Space <bangarshivangi@gmail.com>
2026-05-04 01:56:15 +0530

    Step 3 screen completed

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -5,6 +5,8 @@ import { RootStackParamList } from './types';
 
 import Step1Screen from '../screens/Step1';
 import Step2Screen from '../screens/Step2';
+import Step3Screen from '../screens/Step3';
+import Step4Screen from '../screens/Step4';
 import { COLORS } from '../constants/theme';
 
 const Stack = createStackNavigator<RootStackParamList>();
@@ -29,6 +31,18 @@ const AppNavigator = () => {
             name="Step2"
             component={Step2Screen}
             options={{ title: 'Step 2' }}
+        />
+
+        <Stack.Screen
+            name="Step3"
+            component={Step3Screen}
+            options={{ title: 'Step 3' }}
+        />
+
+        <Stack.Screen
+            name="Step4"
+            component={Step4Screen}
+            options={{ title: 'Step 4' }}
         />
       </Stack.Navigator>
     </NavigationContainer>
