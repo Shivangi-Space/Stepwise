d62952dee619437d628900b34d3467f6dd6e65c2
Shivangi-Space <bangarshivangi@gmail.com>
2026-05-04 00:05:58 +0530

    added types

---
+++ src/navigation/types.ts
@@ -0,0 +1,7 @@
+export type RootStackParamList = {
+    Step1: undefined;
+    Step2: undefined;
+    Step3: undefined;
+    Step4: undefined;
+    Summary: undefined;
+}
---
+++ src/screens/Step1.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from "react-native";
+import Container from "../components/Container";
+
+const Step1Screen = () => {
+    return(
+        <View>
+            <Text>Step 1</Text>
+        </View>
+    )
+}
+
+export default Step1Screen;
